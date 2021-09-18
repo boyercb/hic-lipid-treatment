0d4687f924a8acf0dd13957a11ec6b5712164e24
Christopher Boyer <christopherbboyer@gmail.com>
2021-09-18 10:57:30 -0400

    Create __master_run.R

---
+++ __master_run.R
@@ -0,0 +1,31 @@
+# Define global analysis parameters ---------------------------------------
+
+
+# Packages and helper functions -------------------------------------------
+
+source("1_code/0_packages.R")
+
+source("1_code/1_helpers.R")
+
+
+# Load and clean data -----------------------------------------------------
+
+source("1_code/2_load_data.R")
+
+source("1_code/3_cleaning.R")
+
+source("1_code/4_exclusion.R")
+
+source("1_code/5_create_variables.R")
+
+
+# Main analysis -----------------------------------------------------------
+
+source("1_code/6_analysis.R")
+
+source("1_code/7_plots.R")
+
+
+# Appendix ----------------------------------------------------------------
+
+source("1_code/X_data_sources.R")
