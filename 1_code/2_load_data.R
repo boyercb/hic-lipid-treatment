f6be3717481afa10638af131a0cb9d7be6fd323f
Christopher Boyer <christopherbboyer@gmail.com>
2021-09-18 10:56:57 -0400

    Create 2_load_data.R

---
+++ 1_code/2_load_data.R
@@ -0,0 +1,6 @@
+# read in data from high income countries from Bin
+hic <- read_rds("0_data/Selected studies with TC and HDL or LDL and treatment.RDS")
+hic <- as_tibble(hic)
+
+# read in meta-data for all NCDRisC surveys
+ncdrisc <- read_csv("0_data/metadata_lipids_20210206_wNmed.csv")
