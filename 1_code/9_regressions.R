0e95076fc53aa84978df7997747f8d6f952dab62
Christopher Boyer <christopherbboyer@gmail.com>
2022-01-28 14:57:01 -0500

    Create 9_regressions.R
    Add fixed effect regressions

---
+++ 1_code/9_regressions.R
@@ -0,0 +1,44 @@
+library(fixest)
+library(modelsummary)
+
+d <- hic
+d$post2010 = ifelse(d$mid_year >= 2010, 1, 0)
+d$woman = as.numeric(d$sex) - 1
+
+fe <- list(
+  "Non-HDL-C" = feols(
+    fml = nonhdl ~ woman + age + woman:post2010 + age:post2010 + woman:age:post2010 | iso + mid_year, 
+    data = d,
+    vcov = "twoway"
+  ),
+  "Eligible" = feols(
+    fml = eligible ~ woman + age + woman:post2010 + age:post2010 + woman:age:post2010 | iso + mid_year, 
+    data = d,
+    subset = !d$missing_rf & !d$missing_chol,
+    vcov = "twoway"
+  ),
+  "Treated" = feols(
+    fml = treated ~ woman + age + woman:post2010 + age:post2010 + woman:age:post2010 | iso + mid_year, 
+    data = d,
+    subset = d$eligible == 1,
+    vcov = "twoway"
+  )
+)
+
+
+# View(hic %>% filter(is.na(treated)) %>%
+#   select(id_study,
+#          Country,
+#          mid_year,
+#          iso,
+#          all_of(c(anthro, lipids, bp)),
+#          sex,
+#          agecont,
+#          age,
+#          smoker,
+#          self_diab))
+
+modelsummary(models = fe,
+             output = "latex",
+             stars = TRUE,
+             gof_omit = "(R2 Within)|(R2 Pseudo)|(AIC)|(BIC)|(Log.Lik.)")
